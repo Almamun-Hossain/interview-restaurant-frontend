d4a127695e3911f46e1e53211452315bfccdc48d
Almamun <almamunhossains@gmail.com>
2023-08-30 18:16:36 +0600

    change cookie policy

--- app/_utils/AppCookiesProvider.js
+++ app/_utils/AppCookiesProvider.js
@@ -3,16 +3,18 @@ import React from "react";
 import { CookiesProvider, useCookies } from "react-cookie";
 
 const AppCookiesProvider = ({ children }) => {
+  // let cookie = new Cookies();
   let { data: session } = useSession();
   let [cookies, setCookies] = useCookies([session]);
   if (session && session.user) {
-    if (!cookies.token)
+    if (!cookies.token) {
       setCookies("token", session.user.token, {
-        path: "/",
         httpOnly: true,
+        path: "/",
         sameSite: "none",
         secure: true,
       });
+    }
   }
 
   return <CookiesProvider>{children}</CookiesProvider>;
